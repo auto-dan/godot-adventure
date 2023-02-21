62cd272e37d957dfd7780d13a01f54369f30ae24
Dan Peterson <8581039+auto-dan@users.noreply.github.com>
2023-02-20 19:11:52 -0500

    camera + bug fixes

--- World/Grass.gd
+++ World/Grass.gd
@@ -9,6 +9,6 @@ func create_grass_effect():
 		get_parent().add_child(grass_effect)
 		grass_effect.global_position = global_position
 
-func _on_Hurtbox_area_entered():
+func _on_Hurtbox_area_entered(area):
 	create_grass_effect()
 	queue_free()
